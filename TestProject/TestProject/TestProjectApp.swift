133da764f5fe328b65aaac9c188783c1d317fa76
HYEJI KO <hy2ji.k5@gmail.com>
2023-04-17 17:42:18 +0900

    프로젝트 파일 생성

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by 고혜지 on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
