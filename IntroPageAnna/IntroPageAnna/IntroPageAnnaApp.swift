ea6a7636ab776dde693365d539fbfc546efb0f64
addlight19 <addlight19@gmail.com>
2023-05-03 16:59:02 +0900

    feat : add Textfield and datePickerWheel

---
+++ IntroPageAnna/IntroPageAnna/IntroPageAnnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroPageAnnaApp.swift
+//  IntroPageAnna
+//
+//  Created by 유빈 on 2023/04/28.
+//
+
+import SwiftUI
+
+@main
+struct IntroPageAnnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
