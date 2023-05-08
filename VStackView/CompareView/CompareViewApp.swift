f5fcf468943ff04c262a35e32198643e682990bb
LIVV23 <nuvee023@gmail.com>
2023-05-09 00:09:00 +0900

    docs : Befor After Compare View

---
+++ VStackView/CompareView/CompareViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VStackViewApp.swift
+//  VStackView
+//
+//  Created by 김규리 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct VStackViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CompareView()
+        }
+    }
+}
